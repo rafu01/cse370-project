f3ae5a62561cf556f55149643bb9523bdd7cb00b
Aditya Roy <54760764+Einstein2193@users.noreply.github.com>
2020-12-03 15:52:42 +0600

    joined database MySql

--- car_showroom/settings.py
+++ car_showroom/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'car_showroom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'gearupdb',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
