e07764e6c9eff284aa3fc9da73d9f62f5f63c19c
rafu01 <nrafsan13@gmail.com>
2020-12-05 13:01:35 +0600

    updated database

--- gearupdb.sql
+++ gearupdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2020 at 09:27 PM
+-- Generation Time: Dec 05, 2020 at 08:00 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
